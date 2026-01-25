b1c0db3b3c20c426083a70d8040f6689d447a078
Ihab McShea <ihabmcshea@Ihabs-MacBook-Air.local>
2026-01-25 07:48:25 +0200

    Add root application module

---
+++ src/app.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { PrismaModule } from './prisma/prisma.module';
+import { TasksModule } from './modules/tasks/tasks.module';
+import { ReferenceModule } from './modules/reference/reference.module';
+import { AttachmentsModule } from './modules/attachments/attachments.module';
+import { AuditModule } from './modules/audit/audit.module';
+import { TenantGuard } from './common/guards/tenant.guard';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    TasksModule,
+    ReferenceModule,
+    AttachmentsModule,
+    AuditModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: TenantGuard,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+  ],
+})
+export class AppModule {}
